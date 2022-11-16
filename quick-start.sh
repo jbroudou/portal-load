85b3e6e7413942bcaaf90158284050dd1d059a4a
krytie <julian.broudou@gmail.com>
2022-11-16 11:14:21 +1100

    .

--- quick-start.sh
+++ quick-start.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 MAX_CPU_CHARES=1024
 MIN_CPU_CHARES=128
-MAX_LOAD_THRESHOLD=80
-MIN_LOAD_THRESHOLD=70
+MAX_LOAD_THRESHOLD=85
+MIN_LOAD_THRESHOLD=80
 DURATION=300
-
-usage="$(basename "$0") [-h] [-d Duration in seconds]"
-options=':hd:'
+OUT_FILE=./results/out.csv
+NO_LIMITING=0
+usage="$(basename "$0") [-h] [-n Don't impose CPU limiting] [-d Duration in seconds] [-o Output file]"
+options=':hnd:o:'
 while getopts $options option; do
   case "$option" in
     h) echo "$usage"; exit;;
+    n) NO_LIMITING=1;;
     d) DURATION=$OPTARG;;
+    o) OUT_FILE=$OPTARG;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2; echo "$usage" >&2; exit 1;;
   esac
 done
 
-# Function that will get executed when the user presses Ctrl+C
+
 function shutdown(){
     echo "Shutting down..."
     docker-compose down
@@ -35,7 +38,7 @@ docker-compose up --force-recreate --build -d
 sleep 10
 
 
-echo "load_perc, portal_shares, portal_cpu_usage, stress_cpu_uasge" > out.csv
+echo "load_perc,portal_shares,seconds,portal_cpu_usage,stress_cpu_uasge,job_exec_time" > $OUT_FILE
 
 SECONDS=0
 while [ $SECONDS -lt $DURATION ]; 
@@ -47,33 +50,55 @@ do
     P=$(($LOAD / $PU))
 
 
-# need to ignore if no data is returned
     PORTAL_CPU_USAGE=$(curl -X POST -s localhost:9090/api/v1/query \
         -H "Content-Type: application/x-www-form-urlencoded" \
-        -d "query=sum by (name) (rate(container_cpu_usage_seconds_total{name='portal'}[1m]))" | jq -r .data.result[0].value[1])
+        -d "query=sum by (name) (rate(container_cpu_usage_seconds_total{name='portal'}[30s]))" | jq -r .data.result[0].value[1])
+    if [ -z "$PORTAL_CPU_USAGE" ]; then
+        continue
+    fi
     STRESS_CONTAINER_CPU_USAGE=$(curl -X POST -s localhost:9090/api/v1/query \
         -H "Content-Type: application/x-www-form-urlencoded" \
-        -d "query=sum by (name) (rate(container_cpu_usage_seconds_total{name='stress-container'}[1m]))" | jq -r .data.result[0].value[1])        
+        -d "query=sum by (name) (rate(container_cpu_usage_seconds_total{name='stress-container'}[30s]))" | jq -r .data.result[0].value[1])        
     PORTAL_CPU_SHARES=$(curl -X POST -s localhost:9090/api/v1/query \
         -H "Content-Type: application/x-www-form-urlencoded" \
         -d "query=container_spec_cpu_shares{name='portal'}" | jq -r .data.result[0].value[1])
 
-    echo "SECONDS $SECONDS | LOAD $LOAD | LOAD PERC $P | PORTAL SHARES $PORTAL_CPU_SHARES | PORTAL_CPU_USAGE $PORTAL_CPU_USAGE | STRESS_CONTAINER_CPU_USAGE $STRESS_CONTAINER_CPU_USAGE"
+    JOB_EXEC_TIME=$(docker exec portal cat /app/time.txt 2>/dev/null)
+
+    echo "SECONDS $SECONDS 
+LOAD $LOAD 
+LOAD % $P 
+PORTAL SHARES $PORTAL_CPU_SHARES 
+PORTAL_CPU_USAGE $PORTAL_CPU_USAGE 
+STRESS_CONTAINER_CPU_USAGE $STRESS_CONTAINER_CPU_USAGE
+
+"
 
     if (( P >= $MAX_LOAD_THRESHOLD && $PORTAL_CPU_SHARES == $MAX_CPU_CHARES  )); then
-        echo "Decreasing Portal CPU shares"
-        #docker update --cpu-shares="$MIN_CPU_CHARES" portal
+        echo "Above or equal to max threshold"
+        if ((NO_LIMITING == 0)); then
+            echo "Decreasing Portal CPU shares"
+            docker update --cpu-shares="$MIN_CPU_CHARES" portal 2>/dev/null
+        fi
     elif (( P < $MIN_LOAD_THRESHOLD && $PORTAL_CPU_SHARES == $MIN_CPU_CHARES  )); then
-        echo "Increasing Portal CPU shares"
-        #docker update --cpu-shares="$MAX_CPU_CHARES" portal
-    else
-        echo "NO CHANGE!!"
+        echo "Below min threshold"
+        if ((NO_LIMITING == 0)); then
+            echo "Increasing Portal CPU shares"
+            docker update --cpu-shares="$MAX_CPU_CHARES" portal 2>/dev/null
+        fi
     fi
 
-    echo "$P, $PORTAL_CPU_SHARES, $PORTAL_CPU_USAGE, $STRESS_CONTAINER_CPU_USAGE" >> out.csv
+    if [ $PORTAL_CPU_USAGE == 'null' ]; then
+        PORTAL_CPU_USAGE=0
+    fi
+    if [ $STRESS_CONTAINER_CPU_USAGE == 'null' ]; then
+        STRESS_CONTAINER_CPU_USAGE=0
+    fi
+    
+
+    echo "$P,$PORTAL_CPU_SHARES,$SECONDS,$PORTAL_CPU_USAGE,$STRESS_CONTAINER_CPU_USAGE,$JOB_EXEC_TIME" >> $OUT_FILE
 
 	sleep 2
 done
-
 
 shutdown
--- stress-container/wait.sh
+++ stress-container/wait.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # wait a bit so that the container can start
-sleep 60
+sleep 30
 
 while :
 do
 	cpus=$(cat cpus)
 	echo "Running stress-ng with $cpus CPUS ..."
-	stress-ng --cpu $cpus --timeout 120s --metrics-brief
-	sleep 120
+	stress-ng --cpu $cpus --timeout 60s --metrics-brief
+	sleep 30
 done
